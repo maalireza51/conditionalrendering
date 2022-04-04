5b8d4f5136e8efbf9a9c42b5d024b693ce49767d
maalireza51 <maalireza51@yahoo.com>
2022-04-04 23:22:12 +0430

    containet and enter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import React from 'react'
 function App() {
   return (
     <div className="App">
-
+      <div className='container'>
+        <span id='enter-form' class='neon'>
+          <h3>My chat app</h3>
+          <form>
+            <input type='text' id="name" placeholder="Write your name . . ." />
+            <input type="submit" value="Enter" />
+          </form>
+        </span>
+      </div>
     </div>
   );
 }
