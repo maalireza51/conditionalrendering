70a7bd2be4940af263840dccee928db3ad21f25c
maalireza51 <maalireza51@yahoo.com>
2022-04-07 01:56:42 +0430

    sidebar contacts eventlistener

---
+++ src/Component/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function contact(props) {
+    return (
+        <div className="s-chats" onClick={props.clickHandler}>
+            <img src="https://cdn5.vectorstock.com/i/1000x1000/07/39/man-avatar-profile-view-vector-22890739.jpg" alt="" />
+            <div>
+                <h4>{props.name}</h4>
+                <p>{props.message}</p>
+            </div>
+        </div>
+    )
+}
--- src/Component/Sidebar.jsx
+++ src/Component/Sidebar.jsx
@@ -2,15 +2,35 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
 import { faGear } from '@fortawesome/free-solid-svg-icons'
+import Contact from './Contact'
 
 export default function Sidebar(props) {
+
+  const contacts = [
+    { name: "Ali", message: "hello.how are you?" },
+    { name: "Mehdi", message: "hello.how are you?" },
+    { name: "Reza", message: "hello.how are you?" }
+  ]
+  const friends = contacts.map(friend => {
+    return <Contact name={friend.name} message={friend.message} clickHandler={clickHandler} />
+  })
+
+  function clickHandler(e) {
+    var elems = document.querySelectorAll(".s-chats");
+    [].forEach.call(elems, function (el) {
+      el.classList.remove("active");
+    });
+    e.target.classList.add('active');
+  }
+
   return (
     <div className='sidebar'>
-        <div className='s-header'>
-            <a href='#' onClick={props.handleLogin}><FontAwesomeIcon icon={faAngleLeft}></FontAwesomeIcon></a>
-            <span>Hello {props.name}</span>
-            <a href='#'><FontAwesomeIcon icon={faGear}></FontAwesomeIcon></a>
-        </div>
+      <div className='s-header'>
+        <a href='#' onClick={props.handleLogin}><FontAwesomeIcon icon={faAngleLeft}></FontAwesomeIcon></a>
+        <span>Hello {props.name}</span>
+        <a href='#'><FontAwesomeIcon icon={faGear}></FontAwesomeIcon></a>
+      </div>
+      {friends}
     </div>
   )
 }
